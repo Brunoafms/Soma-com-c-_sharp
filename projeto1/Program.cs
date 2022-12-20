661f35153a917762b0ac6091a4951690eaf77a82
bruno <bruno@DESKTOP-FDD1KJV>
2022-12-20 14:05:55 -0300

    Adicionar arquivos de projeto.

---
+++ projeto1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro número para ser somado");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo número para ser somado");
+            int b = int.Parse(Console.ReadLine());
+            int soma = a + b;
+            Console.WriteLine("Soma: " + soma);
+
+        }
+    }
+}
+
+    
